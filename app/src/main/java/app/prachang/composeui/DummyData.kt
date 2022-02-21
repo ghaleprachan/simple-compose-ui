d07e1dfc66f9027e478985690c0283388289f6cf
ghaleprachan <ghale.prachan@gmail.com>
2022-02-21 13:08:34 +0545

    [Add] adds animate item placement on lazy colum items

--- app/src/main/java/app/prachang/composeui/DummyData.kt
+++ app/src/main/java/app/prachang/composeui/DummyData.kt
@@ -46,35 +46,35 @@ val hotels = listOf(
     ),
     Hotel(
         "https://cf.bstatic.com/xdata/images/hotel/square200/201118825.webp?k=16e19b9c0a2064f8bd98238b349b52ca72f4e7db722c1d055f2f244f277c3d6f&o=",
-        "Kathmandu Eco Hotel",
+        "Hotel Bauddha",
         "NPR 2,956",
         "95%",
         "Thamel, Kathmandu"
     ),
     Hotel(
         "https://cf.bstatic.com/xdata/images/hotel/square200/173753181.webp?k=430efbf2d51d3e99ba918ada98b27dfd230aed7d26bd67980579ed192a43ec5a&o=",
-        "Hotel Jhampa",
+        "Jimbu Hotel",
         "NPR 3,472",
         "97%",
         "Kapan, Kathmandu"
     ),
     Hotel(
         "https://cf.bstatic.com/xdata/images/hotel/square200/173753181.webp?k=430efbf2d51d3e99ba918ada98b27dfd230aed7d26bd67980579ed192a43ec5a&o=",
-        "Hotel Shanker",
+        "Hyatt Hotel",
         "NPR 6,987",
         "94%",
         "Lazimpat, Kathmandu"
     ),
     Hotel(
         "https://cf.bstatic.com/xdata/images/hotel/square200/37849313.webp?k=edb5ca4db2818228d1bf6b77ce5c87409472b65181270eae3e99c3fff7111e84&o=",
-        "Hotel Maridian Suite",
+        "Shiva Hotel",
         "NPR 3,784",
         "98%",
         "Kathmandu"
     ),
     Hotel(
         "https://cf.bstatic.com/xdata/images/hotel/square200/96594338.webp?k=ae23e8a2c80b83a7aeae15a0c454440cc132c8a8f50c949fc5a839b6bec4f554&o=",
-        "Kathmandu Guest House",
+        "Himalayan Guest House",
         "NPR 5,853",
         "91%",
         "Thamel Kathmandu"
--- app/src/main/java/app/prachang/composeui/MainActivity.kt
+++ app/src/main/java/app/prachang/composeui/MainActivity.kt
@@ -3,15 +3,22 @@ package app.prachang.composeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.*
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ThumbUp
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,6 +26,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import app.prachang.composeui.ui.theme.ComposeUITheme
@@ -41,8 +49,10 @@ private fun MyComposeUIPreview() {
     MyComposeUI()
 }
 
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun MyComposeUI() {
+    var hotelsList by remember { mutableStateOf(hotels) }
     Scaffold(
         backgroundColor = Color(0xff1A1917),
         topBar = {
@@ -58,13 +68,41 @@ fun MyComposeUI() {
             contentPadding = PaddingValues(16.dp),
             verticalArrangement = Arrangement.spacedBy(12.dp),
         ) {
-            items(hotels) { hotel ->
-                HotelItemUI(hotel = hotel)
+            stickyHeader {
+                Surface(color = Color(0xff1A1917)) {
+                    OutlinedButton(
+                        modifier = Modifier.fillMaxWidth(),
+                        onClick = {
+                            hotelsList = hotelsList.shuffled()
+                        },
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            backgroundColor = Color.Transparent
+                        ),
+                        border = BorderStroke(
+                            width = 1.dp,
+                            color = MaterialTheme.colors.primary,
+                        ),
+                    ) {
+                        Text(text = "Shuffle")
+                    }
+                }
+            }
+            itemsIndexed(
+                items = hotelsList,
+                key = { _, item ->
+                    item.hotelName
+                },
+            ) { _, hotel ->
+                HotelItemUI(
+                    modifier = Modifier.animateItemPlacement(
+                        animationSpec = tween(500)
+                    ),
+                    hotel = hotel,
+                )
             }
         }
     }
 }
-
 
 @Preview
 @Composable
@@ -73,12 +111,15 @@ private fun HotelItemUIPreview() {
 }
 
 @Composable
-private fun HotelItemUI(hotel: Hotel) {
+private fun HotelItemUI(
+    modifier: Modifier = Modifier,
+    hotel: Hotel,
+) {
     val painter = rememberImagePainter(
         data = hotel.hotelImage
     )
     Card(
-        modifier = Modifier.wrapContentWidth(),
+        modifier = modifier.wrapContentWidth(),
         backgroundColor = Color(0xff23232D),
         elevation = 8.dp,
     ) {
